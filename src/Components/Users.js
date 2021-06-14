f7a1abf6c9791819ae474f683ed2ace2bb5cba37
Nissrine-Hnainia <hnainianesrine@gmail.com>
2021-06-14 11:34:21 +0100

    initial commit

---
+++ src/Components/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserCard from './UserCard';
+
+const Users = () => {
+    return (
+        <div className="users">
+            <UserCard />
+            <UserCard />
+            <UserCard />
+        </div>
+    )
+}
+
+export default Users
